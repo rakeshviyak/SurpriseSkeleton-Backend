9163ee05f8047ac750a03db2ea52730851df1948
Rakesh Viyak <rakeshviyak@gmail.com>
2015-04-11 13:29:38 +0800

    first commit

---
+++ todo.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template
+import flask.ext.sqlalchemy
+import flask.ext.restless
+
+
+app = Flask(__name__)
+
+# Create the Flask-SQLAlchemy object and an SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    """database model"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, unique=False)
+    is_completed = db.Column(db.Boolean)
+
+    # When we create a new Todo it should be incomplete and have a title
+    def __init__(self, title, is_completed=False):
+        self.title = title
+        self.is_completed = is_completed
+
+
+# Create the database tables.
+db.create_all()
+
+
+# Create the Flask-Restless API manager.
+restless_manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+
+
+def ember_formatter(result, search_params):
+    a = {'todos': result['objects']} if 'page' in result else result
+    app.logger.debug(str(a) + str(search_params))
+    return {'todos': result['objects']} if 'page' in result else result
+
+
+def pre_ember_formatter(results):
+    app.logger.debug(result)
+    app.logger.debug(result['todo'])
+    return result['todo']
+
+
+def pre_patch_ember_formatter(instid, result):
+    return result['todo']
+
+
+# Create API endpoint, which will be available at /api/todos
+restless_manager.create_api(
+    Todo,
+    methods=['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    url_prefix='/api',
+    collection_name='todos',
+    results_per_page=-1,
+    # postprocessors={
+    #     'GET_MANY': [ember_formatter]
+    # },
+    preprocessors={
+        'POST': [pre_ember_formatter],
+        'PUT_SINGLE': [pre_patch_ember_formatter]
+    }
+)
+
+
+# Create API endpoints, which will be available at /api/<tablename> by
+# default. Allowed HTTP methods can be specified as well.
+# restless_manager.create_api(Todo, methods=['GET', 'POST', 'DELETE'])
+
+
+# @app.route('/')
+# def hello_world():
+#     return render_template('index.html')
+
+app.debug = True
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
