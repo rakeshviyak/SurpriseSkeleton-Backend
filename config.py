27d7106dac8c553c4594a12b5cab3a33e57cda6f
rakesh viyak <rakeshviyak@gmail.com>
2015-04-14 14:34:47 +0000

    add prod server configuration

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 DEBUG = True # Turns on debugging features in Flask
-SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root:password@localhost/test'
+SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root:skeleton@localhost/test'
